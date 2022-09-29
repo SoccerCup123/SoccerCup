d88d5ba390cb3076233d7fb68e59d8c320427832
Isaias-Tortorele <isaiastortorele23@gmail.com>
2022-09-28 21:29:51 -0300

    Mudança layout home

--- SoccerCup/app/views/home.php
+++ SoccerCup/app/views/home.php
@@ -1,40 +1,80 @@
-   <header class="masthead bg-primary text-white text-center">
-            <div class="container d-flex align-items-center flex-column">
-               
-                <img class="masthead-avatar mb-5" src="assets/img/avataaars.svg" alt="..." />
-               
-                <h1 class="masthead-heading text-uppercase mb-0">Copa do Mundo</h1>
-                
-                <div class="divider-custom divider-light">
-                    <div class="divider-custom-line"></div>
-                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                    <div class="divider-custom-line"></div>
-                </div>
-                <table class="table table-striped table-bordered table-hover">
-                    <thead>
-                        <tr>
-                            <h1>
-                             Fase de grupos da Copa do Mundo 2022
-                            </h1>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-				            <td>
-                                21/11/2022 – 07:00
-                                Estádio al-Thumama
-                            </td>
-                            <td>
-                                <div class="wp-block-e2-games-overview-table-row games-overview-table__row grid__container"><div class="grid__row"><div class="games-overview-table__column grid__col-6 grid__col-dt-3"><div class="games-overview-table__date date-time" data-match-date-start="2022-11-21T10:00:00Z">21/11/2022 – 07:00</div><div class="games-overview-table__stadium stadium-name"><a href="https://www.futeboleiro.com/copa-do-mundo-2022/estadios-sedes/al-thumama/">Estádio al-Thumama</a></div></div><div class="games-overview-table__column grid__col-6 grid__col-dt-3"><div class="games-overview-table__team-flags games-overview-table__team-flags--national"><span class="games-overview-table__team-image"><img loading="lazy" src="https://sportscubecdn-wqps7yvkz.stackpathdns.com/images/team2fa6461d0e8129f1cb5a08c01921b1d0cd070c5fc5034cb20fb2b50269f4fe83b9b76e695db2b856f0c919e92c933815565155b3421fe5fbec149bbd24aa68c7.svg" class="games-overview-table__team--flag games-overview-table__team--flag--home" alt="Senegal" height="24" width="24"></span><span class="games-overview-table__result match-result">–</span><span class="games-overview-table__team-image"><img loading="lazy" src="https://sportscubecdn-wqps7yvkz.stackpathdns.com/images/team3d2f09ec3f37acad8249e560b0145640d70b0fce8caf094438ee1870d1fb35eb22e20a1637c2069a5cec333dc0d4d38280238afca8bc342adb6304ba523c3ac7.svg" class="games-overview-table__team--flag games-overview-table__team--flag--away" alt="Holanda" height="24" width="24"></span></div><div class="games-overview-table__teams">Senegal vs Holanda</div></div></div></div>
-                            </td>   
-                            <td>
-						        <a href="?i=apostas" class="btn btn-warning btn-sm" role="button" >
-								<i class="fas fa-pencil-alt"></i>&nbsp;Apostar</a>
-							</td>
-						</tr>
-				    </tbody>
-			    </table>       
-                <p class="masthead-subheading font-weight-light mb-0">funcionários mais engajados, felizes e produtivida</p>
-            </div>
-        </header>
- 
+   <header class="masthead text-black text-center">
+     <div class="container d-flex align-items-center flex-column">
+
+       <h1 class="masthead-heading text-uppercase mb-0">Copa do Mundo</h1>
+       <img class="masthead-avatar mb-1" src="./assets/logo.png" alt="logo Soccer Cup" />
+
+       <table class="table table-striped table-bordered table-hover ">
+         <thead>
+           <tr>
+             <h1>
+               Fase de grupos da Copa do Mundo 2022
+             </h1>
+           </tr>
+         </thead>
+
+         <tbody>
+           <tr class="text-center font-weight-bold ">
+             <td class="align-middle">Data</td>
+             <td class="align-middle">Estádio</td>
+             <td class="align-middle">Seleções</td>
+             <td class="align-middle"></td>
+           </tr>
+
+           <tr class="text-center">
+             <td class="align-middle">21/11/2022 – 07:00h</td>
+             <td class="align-middle">Estádio</td>
+             <td class="align-middle">
+               <img src="" class="rounded-circle" alt="Senegal" height="24" width="24">
+               <span>Senegal vs Holanda</span>
+               <img src="" class="rounded-circle" alt="Holanda" height="24" width="24">
+             </td>
+             <td class="align-middle">
+               <a href="?i=apostas" class="btn btn-warning btn-sm" role="button">
+                 <i class="fas fa-pencil-alt"></i>&nbsp;Apostar</a>
+             </td>
+           </tr>
+
+           <tr class="text-center ">
+             <td class="align-middle">21/11/2022 – 07:00</td>
+             <td class="align-middle">Estádio al-Thumama</td>
+             <td class="align-middle">
+               <img loading="lazy" src="https://upload.wikimedia.org/wikipedia/commons/0/01/Brazil_flag_300.png"
+                 class="rounded-circle" alt="" height="24" width="24">
+               <span>Brasil vs São Paulo</span>
+               <img loading="lazy" src="https://www.imagensempng.com.br/wp-content/uploads/2021/01/50.png"
+                 class="rounded-circle" alt="" height="24" width="24">
+             </td>
+             <td class="align-middle">
+               <a href="?i=apostas" class="btn btn-warning btn-sm" role="button">
+                 <i class="fas fa-pencil-alt"></i>&nbsp;Apostar</a>
+             </td>
+           </tr>
+
+           <tr class="text-center ">
+             <td class="align-middle">21/11/2022 – 07:00</td>
+             <td class="align-middle">Estádio al-Thumama</td>
+             <td class="align-middle">
+               <span class="">
+                 <img loading="lazy" src="https://cdn-icons-png.flaticon.com/512/197/197560.png" class="rounded-circle"
+                   alt="" height="24" width="24">
+               </span>
+
+               <span class="">França vs Tunísia</span>
+               <span class="">
+                 <img loading="lazy"
+                   src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Flag_of_Tunisia.svg/2560px-Flag_of_Tunisia.svg.png"
+                   class="rounded-circle" alt="" height="24" width="24">
+               </span>
+
+             </td>
+             <td class="align-middle">
+               <a href="?i=apostas" class="btn btn-warning btn-sm" role="button">
+                 <i class="fas fa-pencil-alt"></i>&nbsp;Apostar</a>
+             </td>
+           </tr>
+         </tbody>
+       </table>
+       <p class="masthead-subheading font-weight-light mb-0">Aposte com seus Amigos!</p>
+     </div>
+   </header>
